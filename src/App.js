57165f112d5aaeb08b33d4f07f45bdc7675d239d
mayank <mayankpruthi123@gmail.com>
2021-06-14 01:43:45 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 	// listStatus is the category of the list i.e done, pending etc
 	let [listStatus, listStatusHandler] = useState([]);
 
-  let statusInput = useRef(null);
+	let statusInput = useRef(null);
 
 	useEffect(() => {
 		const _list = [
@@ -39,39 +39,41 @@ function App() {
 	if (window.innerWidth < 500) {
 		return (
 			<div className="App">
-				<p>This application is not supported on devices having width smaller than 500pixels.</p>
+				<p style={{ color: "white" }}>This application is not supported on devices having width smaller than 500pixels.</p>
 			</div>
 		);
 	}
 
 	// add new list item in already existing status field
 	const newListItemInputHandler = (item, itemStatus) => {
-    let name = item.current.value;
-    item.current.value = null;
-    listHandler([...list, { name, status: itemStatus }]);
+		let name = item.current.value;
+		item.current.value = null;
+		listHandler([...list, { name, status: itemStatus }]);
 	};
 
 	// add new status list
 	const newStatusListInputHandler = (name) => {
-    statusInput.current.value = null;
+		statusInput.current.value = null;
 		listStatusHandler([...listStatus, name]);
 	};
 
 	// event handlers
-	const handleDragStart = (e, itemName) => {
-		e.dataTransfer.setData("id", itemName);
+	const handleDragStart = (e, itemName, itemIndex) => {
+		e.dataTransfer.setData("name", itemName);
+		e.dataTransfer.setData("index", itemIndex);
 	};
 
 	const handleDragOver = (e) => {
 		e.preventDefault();
 	};
 
-	const handleDrop = (e, status) => {
-		let id = e.dataTransfer.getData("id");
-		console.log(list);
+	const handleDrop = (e, status, index) => {
+		let name = e.dataTransfer.getData("name");
+		console.log(name);
 		let _list = list.filter((item) => {
-			if (item.name === id) {
+			if (item.name === name) {
 				item.status = status;
+			} else if (item.status === status && item.index !== index) {
 			}
 			return item;
 		});
@@ -87,7 +89,7 @@ function App() {
 
 	list.forEach((item, _index) => {
 		listItemsSorted[item.status].push(
-			<div key={_index} className="itemCard" onDragStart={(e) => handleDragStart(e, item.name)} draggable>
+			<div className="itemCard" onDragStart={(e) => handleDragStart(e, item.name, item.index)} draggable>
 				{item.name}
 			</div>,
 		);
